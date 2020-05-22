ae8cc5c6cfeeae7fc18180678ff37a6f427eeec3
Wilmer <amalejandrea@gmail.com>
2020-05-22 10:13:41 -0400

    Adding interactive benefit list

---
+++ app/scripts/utils/benefitTab.js
@@ -0,0 +1,24 @@
+$(function () {
+  const $interactiveBtn = $('#ubimed-benefit__interactiveBtn');
+  const $noInteractiveBtn = $('#ubimed-benefit__noInteractiveBtn');
+  const $interactiveList = $('#ubimed-benefit__interactiveList');
+  const $noInteractiveList = $('#ubimed-benefit__noInteractiveList');
+
+  $interactiveBtn.on('click', () => {
+    if (!$interactiveBtn.hasClass('ubimed-benefit__tabTitle--active')) {
+      $interactiveList.removeClass('d-none');
+      $noInteractiveList.addClass('d-none');
+      $interactiveBtn.addClass('ubimed-benefit__tabTitle--active');
+      $noInteractiveBtn.removeClass('ubimed-benefit__tabTitle--active');
+    }
+  });
+
+  $noInteractiveBtn.on('click', () => {
+    if (!$noInteractiveBtn.hasClass('ubimed-benefit__tabTitle--active')) {
+      $interactiveList.addClass('d-none');
+      $noInteractiveList.removeClass('d-none');
+      $interactiveBtn.removeClass('ubimed-benefit__tabTitle--active');
+      $noInteractiveBtn.addClass('ubimed-benefit__tabTitle--active');
+    }
+  });
+});
